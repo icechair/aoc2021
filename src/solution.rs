ffd3190e7a5d9d05c8fcba9798cf4cf8f67ca767
Martin Gabel <martin.gabel@icechair.de>
2021-12-02 11:13:02 +0100

    with arrays, as length is fixed

--- src/solution.rs
+++ src/solution.rs
@@ -1,62 +1,61 @@
 enum Command {
   Forward(i64),
-  Depth(i64),
+  Aim(i64),
 }
 
 fn parse_command(line: &str) -> Command {
   let mut parts = line.trim().split_ascii_whitespace();
-  let cmd = parts.next();
-  let value = match parts.next() {
-    None => panic!("invalid cmd, no value: {}", line),
-    Some(num) => match num.parse::<i64>() {
-      Err(e) => panic!("invalid cmd value: {}", e),
-      Ok(v) => v,
-    },
-  };
+  let cmd = parts.next().expect("line should not be empty");
+  let value = parts
+    .next()
+    .expect("value should not be empty")
+    .parse::<i64>()
+    .expect("value should be a number");
   match cmd {
-    None => panic!("invalid cmd, empty string"),
-    Some(c) => match c {
-      "forward" => Command::Forward(value),
-      "down" => Command::Depth(value),
-      "up" => Command::Depth(-value),
-      _ => panic!("invalid cmd: {}", c),
-    },
+    "forward" => Command::Forward(value),
+    "down" => Command::Aim(value),
+    "up" => Command::Aim(-value),
+    _ => panic!("invalid cmd: {}", cmd),
   }
 }
 
-fn run_command_v1(position: &mut Vec<i64>, cmd: &Command) {
+fn run_command_v1(sub: &mut [i64; 2], cmd: &Command) {
+  let [ref mut hpos, ref mut depth] = sub;
   match cmd {
-    Command::Forward(value) => position[0] += value,
-    Command::Depth(value) => position[1] += value,
+    Command::Forward(value) => *hpos += value,
+    Command::Aim(value) => *depth += value,
   }
 }
 
-fn run_command_v2(position: &mut Vec<i64>, cmd: &Command) {
+fn run_command_v2(position: &mut [i64; 3], cmd: &Command) {
+  let [ref mut hpos, ref mut depth, ref mut aim] = position;
   match cmd {
-    Command::Depth(value) => position[2] += value,
+    Command::Aim(value) => *aim += value,
     Command::Forward(value) => {
-      position[0] += value;
-      position[1] += position[2] * value;
+      *hpos += value;
+      *depth += *aim * value;
     }
   }
 }
 
 pub fn part1(input: &str) -> String {
-  let mut position = vec![0, 0];
+  let mut submarine = [0; 2];
   for line in input.trim().lines() {
     let cmd = parse_command(line);
-    run_command_v1(&mut position, &cmd);
+    run_command_v1(&mut submarine, &cmd);
   }
-  format!("{}", position[0] * position[1])
+  let [hpos, depth] = submarine;
+  format!("{}", hpos * depth)
 }
 
 pub fn part2(input: &str) -> String {
-  let mut position = vec![0, 0, 0];
+  let mut submarine = [0; 3];
   for line in input.trim().lines() {
     let cmd = parse_command(line);
-    run_command_v2(&mut position, &cmd);
+    run_command_v2(&mut submarine, &cmd);
   }
-  format!("{}", position[0] * position[1])
+  let [hpos, depth, _] = submarine;
+  format!("{}", hpos * depth)
 }
 
 #[cfg(test)]
