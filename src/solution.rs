472cc9c3cdc0f0655a5e0e109e864a3e74d464aa
Martin Gabel <martin.gabel@icechair.de>
2021-12-05 11:40:04 +0100

    part 2 done

--- src/solution.rs
+++ src/solution.rs
@@ -1,10 +1,20 @@
 use std::collections::HashMap;
 use std::fmt;
+use std::ops;
 
 #[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct Point {
   x: i64,
   y: i64,
+}
+
+impl ops::Add for &Point {
+  type Output = Point;
+  fn add(self, rhs: &Point) -> Point {
+    let x = self.x + rhs.x;
+    let y = self.y + rhs.y;
+    return Point { x, y };
+  }
 }
 
 impl Point {
@@ -33,28 +43,35 @@ struct Line {
 
 struct LinePoints<'a> {
   line: &'a Line,
-  current: Point,
+  direction: Point,
+  cursor: Point,
 }
 impl<'a> Iterator for LinePoints<'a> {
   type Item = Point;
   fn next(&mut self) -> Option<Self::Item> {
-    if self.line.is_vertical() {
-      if self.current.y > self.line.to.y {
+    if self.direction.x == 1 {
+      if self.cursor.x > self.line.to.x {
         return None;
       }
-      let next = self.current.clone();
-      self.current.y += 1;
-      return Some(next);
-    } else if self.line.is_horizontal() {
-      //vertical line
-      if self.current.x > self.line.to.x {
-        return None;
-      }
-      let next = self.current.clone();
-      self.current.x += 1;
-      return Some(next);
     }
-    return None;
+    if self.direction.x == -1 {
+      if self.cursor.x < self.line.to.x {
+        return None;
+      }
+    }
+    if self.direction.y == 1 {
+      if self.cursor.y > self.line.to.y {
+        return None;
+      }
+    }
+    if self.direction.y == -1 {
+      if self.cursor.y < self.line.to.y {
+        return None;
+      }
+    }
+    let next = self.cursor.clone();
+    self.cursor = &self.cursor + &self.direction;
+    return Some(next);
   }
 }
 
@@ -67,20 +84,29 @@ impl fmt::Debug for Line {
 impl Line {
   fn from(text: &str) -> Line {
     let mut points = text.trim().split("->");
-    let a = Point::from(points.next().expect("from shoudnt be empty"));
-    let b = Point::from(points.next().expect("from shoudnt be empty"));
-    let mut flip = false;
-
-    if b.x < a.x {
-      flip = true;
-    } else if b.y < a.y {
-      flip = true;
-    }
-    let (from, to) = match flip {
-      true => (b, a),
-      false => (a, b),
-    };
+    let from = Point::from(points.next().expect("from shoudnt be empty"));
+    let to = Point::from(points.next().expect("from shoudnt be empty"));
     return Line { from, to };
+  }
+
+  fn direction(&self) -> Point {
+    let mut x = self.to.x - self.from.x;
+    let mut y = self.to.y - self.from.y;
+    if x > 0 {
+      x = 1;
+    } else if x < 0 {
+      x = -1;
+    } else {
+      x = 0;
+    }
+    if y > 0 {
+      y = 1;
+    } else if y < 0 {
+      y = -1;
+    } else {
+      y = 0;
+    }
+    return Point { x, y };
   }
 
   fn is_horizontal(&self) -> bool {
@@ -98,26 +124,50 @@ impl Line {
   fn points<'a>(&'a self) -> LinePoints<'a> {
     return LinePoints {
       line: &self,
-      current: self.from,
+      direction: self.direction(),
+      cursor: self.from,
     };
-  }
-
-  fn is_point_on_line(&self, point: &Point) -> bool {
-    self.points().any(|p| point == &p)
   }
 }
 
+fn print_map(points: &HashMap<Point, i64>) {
+  let min_x = points
+    .keys()
+    .fold(i64::MAX, |acc, c| if acc > c.x { c.x } else { acc });
+  let max_x = points
+    .keys()
+    .fold(0, |acc, c| if acc < c.x { c.x } else { acc });
+  let min_y = points
+    .keys()
+    .fold(i64::MAX, |acc, c| if acc > c.y { c.y } else { acc });
+  let max_y = points
+    .keys()
+    .fold(0, |acc, c| if acc < c.y { c.y } else { acc });
+
+  let mut output = String::new();
+  for y in min_y..max_y + 1 {
+    for x in min_x..max_x + 1 {
+      output.push_str(&match points.get(&Point { x, y }) {
+        Some(v) => format!("{}", v),
+        None => format!("."),
+      })
+    }
+    output.push('\n');
+  }
+  println!("{}", output.trim_end())
+}
+
 fn count_overlaps(lines: &mut Vec<Line>) -> usize {
-  let mut pointMap: HashMap<Point, i64> = HashMap::new();
+  let mut point_map: HashMap<Point, i64> = HashMap::new();
 
   for line in lines {
     for point in line.points() {
-      let e = pointMap.entry(point).or_insert(0);
+      let e = point_map.entry(point).or_insert(0);
       *e += 1;
     }
   }
-
-  return pointMap.iter().filter(|&(_, v)| v > &1).count();
+  print_map(&point_map);
+  return point_map.iter().filter(|&(_, v)| v > &1).count();
 }
 
 pub fn part1(input: &str) -> String {
@@ -131,8 +181,10 @@ pub fn part1(input: &str) -> String {
   return format!("{}", count_overlaps(&mut lines));
 }
 
-pub fn part2(_input: &str) -> String {
-  return format!("{}", 0);
+pub fn part2(input: &str) -> String {
+  let mut lines: Vec<Line> = input.trim().lines().map(|l| Line::from(l)).collect();
+
+  return format!("{}", count_overlaps(&mut lines));
 }
 
 #[cfg(test)]
@@ -162,12 +214,17 @@ mod test {
     let line = Line::from("8,0 -> 0,8");
     assert_eq!(line.is_horizontal(), false);
     assert_eq!(line.is_vertical(), false);
-    assert_eq!(line.points().count(), 0);
+    assert_eq!(line.points().count(), 9);
 
     let line = Line::from("7,0 -> 7,4");
     assert_eq!(line.is_horizontal(), false);
     assert_eq!(line.is_vertical(), true);
     assert_eq!(line.points().count(), 5);
+
+    let line = Line::from("5,5 -> 8,2");
+    assert_eq!(line.is_horizontal(), false);
+    assert_eq!(line.is_vertical(), false);
+    assert_eq!(line.points().count(), 4);
   }
 
   #[test]
@@ -177,6 +234,6 @@ mod test {
 
   #[test]
   fn test_p2() {
-    assert_eq!(&part2(INPUT), "0");
+    assert_eq!(&part2(INPUT), "12");
   }
 }
