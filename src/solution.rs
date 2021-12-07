0651c5d3aebc9978a8685fa706080b8504909c80
Martin Gabel <martin.gabel@icechair.de>
2021-12-07 10:17:56 +0100

    part 1 done

--- src/solution.rs
+++ src/solution.rs
@@ -1,5 +1,25 @@
-pub fn part1(_input: &str) -> String {
-  return format!("{}", 0);
+pub fn fuel_cost(crabs: &[i64], pos: i64) -> i64 {
+  let mut cost = 0;
+  for crab in crabs {
+    cost += i64::abs(pos - crab);
+  }
+  return cost;
+}
+
+pub fn part1(input: &str) -> String {
+  let mut crabs: Vec<i64> = input.trim().split(',').flat_map(|x| x.parse()).collect();
+  crabs.sort();
+  let min = *crabs.first().unwrap();
+  let max = *crabs.last().unwrap();
+  let mut cost = i64::MAX;
+  for pos in min..max + 1 {
+    let current = fuel_cost(&crabs, pos);
+    if current < cost {
+      cost = current;
+    }
+  }
+
+  return format!("{}", cost);
 }
 
 pub fn part2(_input: &str) -> String {
@@ -11,11 +31,12 @@ mod test {
   use super::*;
 
   const INPUT: &str = "\
+  16,1,2,0,4,2,7,1,2,14
 ";
 
   #[test]
   fn test_p1() {
-    assert_eq!(&part1(INPUT), "0");
+    assert_eq!(&part1(INPUT), "37");
   }
 
   #[test]
