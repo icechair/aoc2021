453f4a2452da6a2776f0bf57983cd2d17d31f91a
Martin Gabel <martin.gabel@icechair.de>
2021-12-07 10:27:40 +0100

    part 2 done

--- src/solution.rs
+++ src/solution.rs
@@ -1,7 +1,24 @@
-pub fn fuel_cost(crabs: &[i64], pos: i64) -> i64 {
+fn naive_fuel_cost(crabs: &[i64], pos: i64) -> i64 {
   let mut cost = 0;
-  for crab in crabs {
+  for &crab in crabs {
     cost += i64::abs(pos - crab);
+  }
+  return cost;
+}
+
+fn crab_fuel_cost(from: i64, to: i64) -> i64 {
+  let delta = i64::abs(to - from);
+  let mut cost = 0;
+  for i in 0..delta + 1 {
+    cost += i;
+  }
+  return cost;
+}
+
+fn pattern_fuel_cost(crabs: &[i64], pos: i64) -> i64 {
+  let mut cost = 0;
+  for &crab in crabs {
+    cost += crab_fuel_cost(crab, pos);
   }
   return cost;
 }
@@ -13,7 +30,7 @@ pub fn part1(input: &str) -> String {
   let max = *crabs.last().unwrap();
   let mut cost = i64::MAX;
   for pos in min..max + 1 {
-    let current = fuel_cost(&crabs, pos);
+    let current = naive_fuel_cost(&crabs, pos);
     if current < cost {
       cost = current;
     }
@@ -22,8 +39,20 @@ pub fn part1(input: &str) -> String {
   return format!("{}", cost);
 }
 
-pub fn part2(_input: &str) -> String {
-  return format!("{}", 0);
+pub fn part2(input: &str) -> String {
+  let mut crabs: Vec<i64> = input.trim().split(',').flat_map(|x| x.parse()).collect();
+  crabs.sort();
+  let min = *crabs.first().unwrap();
+  let max = *crabs.last().unwrap();
+  let mut cost = i64::MAX;
+  for pos in min..max + 1 {
+    let current = pattern_fuel_cost(&crabs, pos);
+    if current < cost {
+      cost = current;
+    }
+  }
+
+  return format!("{}", cost);
 }
 
 #[cfg(test)]
@@ -35,12 +64,18 @@ mod test {
 ";
 
   #[test]
+  fn test_fuel_cost() {
+    assert_eq!(crab_fuel_cost(1, 5), 10);
+    assert_eq!(crab_fuel_cost(2, 5), 6);
+  }
+
+  #[test]
   fn test_p1() {
     assert_eq!(&part1(INPUT), "37");
   }
 
   #[test]
   fn test_p2() {
-    assert_eq!(&part2(INPUT), "0");
+    assert_eq!(&part2(INPUT), "168");
   }
 }
