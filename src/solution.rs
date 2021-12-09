6d41efbf2c6c02c6f96f07f355a5d8df1ce7fdae
Martin Gabel <martin.gabel@icechair.de>
2021-12-09 09:10:09 +0100

    part 1 done (very slow ...)

--- src/solution.rs
+++ src/solution.rs
@@ -1,5 +1,39 @@
-pub fn part1(_input: &str) -> String {
-  return format!("{}", 0);
+mod index;
+use index::*;
+fn parse_heightmap(input: &str) -> (Vec<u8>, usize, usize) {
+  let mut heightmap = Vec::with_capacity(input.trim().len());
+  let mut width = 0;
+  let mut height = 0;
+  for line in input.trim().lines() {
+    height += 1;
+    width = line.trim().len();
+    for b in line.trim().bytes() {
+      heightmap.push(b - b'0');
+    }
+  }
+  return (heightmap, width, height);
+}
+
+pub fn part1(input: &str) -> String {
+  let (mut heightmap, width, height) = parse_heightmap(input);
+  let mut low_points: Vec<u8> = Vec::new();
+  for (ida, h_a) in heightmap.iter().enumerate() {
+    let mut is_lowest = true;
+    for (idb, h_b) in index::neighbours(&heightmap, ida, width, height) {
+      if h_a >= h_b {
+        is_lowest = false;
+      }
+    }
+    if is_lowest {
+      low_points.push(*h_a);
+    }
+  }
+  return format!(
+    "{}",
+    low_points
+      .iter()
+      .fold(0 as usize, |acc, &c| acc + 1 + usize::from(c))
+  );
 }
 
 pub fn part2(_input: &str) -> String {
@@ -11,11 +45,16 @@ mod test {
   use super::*;
 
   const INPUT: &str = "\
+2199943210
+3987894921
+9856789892
+8767896789
+9899965678
 ";
 
   #[test]
   fn test_p1() {
-    assert_eq!(&part1(INPUT), "0");
+    assert_eq!(&part1(INPUT), "15");
   }
 
   #[test]
--- src/solution/index.rs
+++ src/solution/index.rs
@@ -1,9 +1,43 @@
+use std::fmt::Debug;
 pub fn index(row: usize, col: usize, width: usize) -> usize {
   row * width + col
 }
 
-pub fn row_col(index: usize, width: usize, height: usize) -> (usize, usize) {
-  let row = index / height;
+pub fn row_col(index: usize, width: usize) -> (usize, usize) {
+  let row = index / width;
   let col = index % width;
   return (row, col);
+}
+
+pub fn neighbours<T>(
+  list: &[T],
+  id: usize,
+  width: usize,
+  height: usize,
+) -> impl Iterator<Item = (usize, &T)> + '_
+where
+  T: Debug,
+{
+  let (row, col) = row_col(id, width);
+
+  let mut nexts: Vec<usize> = Vec::new();
+  if row > 0 {
+    //up
+    nexts.push(index(row - 1, col, width));
+  }
+  if col > 0 {
+    //left
+    nexts.push(index(row, col - 1, width));
+  }
+  let right = index(row, col + 1, width);
+  if right < list.len() {
+    nexts.push(right);
+  }
+  let down = index(row + 1, col, width);
+  if down < list.len() {
+    nexts.push(down);
+  }
+  return list.iter().enumerate().filter(move |(i, _)| {
+    return *i != id && nexts.contains(i);
+  });
 }
